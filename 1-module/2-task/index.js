e29649f5e172656f61108b227a7f36d8fdf497a8
enrate <greschnikoff@gmail.com>
2021-11-29 20:33:24 +0300

    Валидация

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,8 +10,11 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
-}
+  if(!name || name.match(' ') != null || name.length <= 3){
+    return false;
+  } else
+    return true;
+  }
 
 function sayHello() {
   let userName = prompt('Введите ваше имя');
