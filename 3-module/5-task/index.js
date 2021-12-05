8c6c463ee7682a677e18ff3ad8d9019135e7ca63
enrate <greschnikoff@gmail.com>
2021-12-06 02:13:43 +0300

    3 модуль

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,12 @@
 function getMinMax(str) {
-  // ваш код...
+  let ars = str.split(' ');
+  for (let i = ars.length - 1; i >= 0; i--) {
+    if (ars[i].match(/[^0-9]+$/) != null) {
+      ars.splice([i], 1);
+    }
+  }
+  return str = {
+    'min': min = Math.min.apply(Math, ars),
+    'max': max = Math.max.apply(Math, ars),
+  }
 }
